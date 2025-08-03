472cca7b4c9ab8fc8dd702860159a2dc4f8976ce
Dmytriy <dmytriy4565@gmail.com>
2025-08-03 14:13:50 +0200

    added login input verification

--- quiz-frontend/src/components/login-form.tsx
+++ quiz-frontend/src/components/login-form.tsx
@@ -1,18 +1,31 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type LoginFields = {
+  email: string;
+  password: string;
+};
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFields>();
+
+  const onSubmit: SubmitHandler<LoginFields> = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -20,16 +33,22 @@ export function LoginForm({
           <CardTitle>Login to your account</CardTitle>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="flex flex-col gap-6">
               <div className="grid gap-3">
                 <Label htmlFor="email">Email</Label>
                 <Input
-                  id="email"
+                  {...register("email", {
+                    required: "Email is required",
+                    validate: (value) =>
+                      /^\S+@\S+\.\S+$/.test(value) || "Invalid email format",
+                  })}
                   type="email"
                   placeholder="m@example.com"
-                  required
                 />
+                {errors.email && (
+                  <div className="text-red-600">{errors.email.message}</div>
+                )}
               </div>
               <div className="grid gap-3">
                 <div className="flex items-center">
@@ -41,7 +60,15 @@ export function LoginForm({
                     Forgot your password?
                   </a>
                 </div>
-                <Input id="password" type="password" required />
+                <Input
+                  {...register("password", {
+                    required: "Password is required",
+                  })}
+                  type="password"
+                />
+                {errors.password && (
+                  <div className="text-red-600">{errors.password.message}</div>
+                )}
               </div>
               <div className="flex flex-col gap-3">
                 <Button type="submit" className="w-full">
@@ -56,5 +83,5 @@ export function LoginForm({
         </CardContent>
       </Card>
     </div>
-  )
+  );
 }
