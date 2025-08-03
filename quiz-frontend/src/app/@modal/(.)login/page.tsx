9a0b910c2018dcdbe14f0a787689909ce786d243
Dmytriy <dmytriy4565@gmail.com>
2025-08-03 12:10:04 +0200

    added login ui

---
+++ quiz-frontend/src/app/@modal/(.)login/page.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from '@/components/login-form'
+import Modal from '@/components/modal'
+import React from 'react'
+
+export default function LoginModal() {
+  return (
+    <Modal>
+        intercepted
+        <LoginForm />
+    </Modal>
+  )
+}
